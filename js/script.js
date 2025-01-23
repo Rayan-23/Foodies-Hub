6439a6898a6d519a6972f9da23770a595f032b30
Rayan-23 <renomohamed16@gmail.com>
2025-01-23 09:49:53 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+ 
+    $("#owl-demo").owlCarousel({
+   
+        autoPlay: 3000, 
+   
+        items : 3,
+        itemsDesktop : [1199,3],
+        itemsDesktopSmall : [979,3]
+   
+    });
+   
+  });
+
+
+ 
